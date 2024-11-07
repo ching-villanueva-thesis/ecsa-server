38a5183e0efd93dd4c52cd130df01e0c67baf1a3
chaaals <charlesching0808@gmail.com>
2024-11-07 20:03:50 +0800

    feat: add unimodal and multimodal benchmark functions

--- functions/multimodal.py
+++ functions/multimodal.py
@@ -1,7 +1,13 @@
+import numpy as np
+
 # F3
-def ackley():
-    pass
+def ackley(n = [0,0]):
+    n = np.array(n)
+    return -20 * np.exp(-0.2 * np.sqrt(1/len(n) * np.sum(n**2))) - np.exp(1/len(n) * np.sum(np.cos(2*np.pi*n))) + 20 + np.e
 
 # F4
-def griewank():
-    pass
+def griewank(n = [0,0]):
+    n = np.array(n)
+    term1 = np.sum(n**2) / 4000
+    term2 = np.prod(np.cos(n / np.sqrt(np.arange(1, len(n) + 1))))
+    return term1 - term2 + 1
--- functions/unimodal.py
+++ functions/unimodal.py
@@ -1,7 +1,14 @@
+import numpy as np
+
 # F1
-def sphere():
-    pass
+def sphere(n = [0,0]):
+    n = np.array(n)
+    func_value = 0
+    for i in range(0, len(n)):
+        func_value = func_value + n[i]**2
+    return func_value
 
 # F2
-def rosenbrocks():
-    pass
+def rosenbrocks(n = [0,0]):
+     n = np.array(n)
+     return np.sum(100 * (n[1:] - n[:-1]**2)**2 + (n[:-1] - 1)**2)
